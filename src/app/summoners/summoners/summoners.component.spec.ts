a4afe4e3e757b79f8f4fc89a23ed757c4c3945c2
Anes Smajic <Anes.Smajic@adesso.at>
2023-06-01 14:33:03 +0200

    feat: adjust testcase for summoners component

--- src/app/summoners/summoners/summoners.component.spec.ts
+++ src/app/summoners/summoners/summoners.component.spec.ts
@@ -1,14 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { InjectionToken, Injector } from '@angular/core';
-import { CookieService, CookieOptionsProvider, CookieOptions, COOKIE_WRITER } from 'ngx-cookie';
-import { LoginComponent } from 'src/app/login/login/login.component';
-import { AuthService } from 'src/app/login/auth.service';
-import { RouterTestingModule } from '@angular/router/testing';
 import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
-const COOKIE_OPTIONS: InjectionToken<CookieOptions> = new InjectionToken<CookieOptions>('COOKIE_OPTIONS');
+import { SummonersComponent } from './summoners.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { SummonersService } from '../summoners.service';
+import { AuthService } from 'src/app/login/auth.service';
+import {
+  COOKIE_WRITER,
+  CookieOptions,
+  CookieOptionsProvider,
+  CookieService,
+} from 'ngx-cookie';
+import { InjectionToken, Injector } from '@angular/core';
+
+const COOKIE_OPTIONS: InjectionToken<CookieOptions> =
+  new InjectionToken<CookieOptions>('COOKIE_OPTIONS');
 
 const mockCookieOptions: CookieOptions = {
   path: '/',
@@ -20,23 +28,37 @@ const mockCookieOptions: CookieOptions = {
   storeUnencoded: false,
 };
 
-describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
-  let activatedRoute: Partial<ActivatedRoute>;
+const mockActivatedRoute = {
+  snapshot: {
+    paramMap: {
+      get: (name: string) => 'test',
+    },
+  },
+};
+
+describe('SummonersComponent', () => {
+  let component: SummonersComponent;
+  let fixture: ComponentFixture<SummonersComponent>;
 
   beforeEach(async () => {
-
     await TestBed.configureTestingModule({
-      declarations: [LoginComponent],
-      imports: [FormsModule, HttpClientTestingModule, RouterTestingModule],
+      imports: [HttpClientTestingModule, RouterTestingModule],
+      declarations: [SummonersComponent],
       providers: [
+        SummonersService,
         CookieService,
         AuthService,
         {
           provide: CookieOptionsProvider,
           useFactory: () =>
-            new CookieOptionsProvider(mockCookieOptions, TestBed.inject(Injector)),
+            new CookieOptionsProvider(
+              mockCookieOptions,
+              TestBed.inject(Injector)
+            ),
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: mockActivatedRoute,
         },
         {
           provide: COOKIE_OPTIONS,
@@ -46,14 +68,10 @@ describe('LoginComponent', () => {
           provide: COOKIE_WRITER,
           useValue: {},
         },
-        {
-          provide: ActivatedRoute,
-          useValue: activatedRoute,
-        },
       ],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(LoginComponent);
+    fixture = TestBed.createComponent(SummonersComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/summoners/summoners/summoners.component.ts
+++ src/app/summoners/summoners/summoners.component.ts
@@ -8,26 +8,28 @@ import {map} from "rxjs";
 @Component({
   selector: 'app-summoners',
   templateUrl: './summoners.component.html',
-  styleUrls: ['./summoners.component.scss']
+  styleUrls: ['./summoners.component.scss'],
 })
 export class SummonersComponent implements OnInit {
   summoner: Summoner | undefined;
 
-  constructor(private http: HttpClient,
-              private summonersService: SummonersService,
-              private route: ActivatedRoute) {
-  }
+  constructor(
+    private http: HttpClient,
+    private summonersService: SummonersService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.route.paramMap
-      .pipe(map(() => window.history.state))
-      .subscribe(data => {
-          this.summonersService.querySummoner(data.name)
+    if (this.route.paramMap) {
+      this.route.paramMap
+        .pipe(map(() => window.history.state))
+        .subscribe((data) => {
+          this.summonersService
+            .querySummoner(data.name)
             .subscribe((response) => {
-                this.summoner = response;
-              }
-            )
-        }
-      );
+              this.summoner = response;
+            });
+        });
+    }
   }
 }
